1a5e4602dc611b37d6b1325330eded74b79d3673
Brandon Bakus <brandonbakus@gmail.com>
2025-01-12 12:04:51 -0500

    tricksy tricksy

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+
+
+
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {  
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({ 
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        document.body.textContent = data.id;
+        return data.id;
+    })
+    .catch(error => {
+        document.body.textContent = error.message;
+        error.message = "Unauthorized Access"
+    });
+}
+
+submitData()
